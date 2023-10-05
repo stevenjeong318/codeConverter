b93d55bba0c7469f25decc295b8c447414dcd484
Steven <sungmoon.jeong@mail.mcgill.ca>
2023-10-05 15:11:38 -0400

    new endpoint /done after code converted

--- app.py
+++ app.py
@@ -2,22 +2,25 @@ from flask import Flask
 from flask import render_template
 from flask import request
 from flask import flash
-from flask import redirect
-from flask import url_for
+
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
     return render_template('index.html')
 
-@app.route('/', methods=['POST'])
-def convert():
+
+@app.route('/done', methods=['GET', 'POST'])
+def result():
     in_lang = request.form['dropdown1']
+    in_opt = request.args.get('stored_dropdown_value1', default=None)
     out_lang = request.form['dropdown2']
+    out_opt = request.args.get('stored_dropdown_value2', default=None)
     code = request.form['input-code']
-    r = f"You converted \n {code} \n from {in_lang} to {out_lang}"
-    return r
+    s= f"Convert \n {code} \n from {in_lang} to {out_lang}"
+    print(s)
+    return render_template('result.html', in_code = in_opt, out_code = out_opt, input_code = code, output_code = s)
     
 
 if __name__ == '__main__':
