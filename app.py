e7064e4f46a62d15b9dc04568a9595212e99c339
Steven <sungmoon.jeong@mail.mcgill.ca>
2023-10-04 23:47:55 -0400

    implement app.py retreive input

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
 from flask import Flask
+from flask import render_template
+from flask import request
+from flask import flash
+from flask import redirect
+from flask import url_for
+
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello, World!'
+    return render_template('index.html')
 
-
+@app.route('/', methods=['POST'])
+def convert():
+    in_lang = request.form['dropdown1']
+    out_lang = request.form['dropdown2']
+    code = request.form['input-code']
+    r = f"You converted \n {code} \n from {in_lang} to {out_lang}"
+    return r
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
