8b4de018429635fd1c70e94828cdd5dc472ea6df
Steven <sungmoon.jeong@mail.mcgill.ca>
2023-10-07 15:48:22 -0400

    used gpt api to show converted code output

--- app.py
+++ app.py
@@ -1,24 +1,37 @@
 from flask import Flask
 from flask import render_template
 from flask import request
+import openai
+import json
 
 app = Flask(__name__)
 
+#load api keys from json file
+with open('config.json', 'r') as config_file:
+    data = json.load(config_file)
+openai.api_key = data['openai_api_key']
 
+#serves home page
 @app.route('/', methods=['GET'])
 def home():
     return render_template('index.html')
 
-
+#serves converted page with endpoint done
 @app.route('/done', methods=['GET', 'POST'])
 def result():
-    dropdown1_value = request.form.get('dropdown1')
-    dropdown2_value = request.form.get('dropdown2')
-    same_value = dropdown1_value == dropdown2_value
-    code = request.form['input-code']
+    dropdown1_value = request.form.get('dropdown1') #saves choice of 1st dropdown from index.html
+    dropdown2_value = request.form.get('dropdown2') #saves choice of 2nd dropdown from index.html
+    same_value = dropdown1_value == dropdown2_value #bool variable checking if 1st (input lang) and 2nd (output lang) are same
+    code = request.form['input-code'] #saves input code from index.html
     s = f"Convert \n {code} \n from {dropdown1_value} to {dropdown2_value}"
-    print(dropdown1_value, dropdown2_value)
-    return render_template('result.html', bool=same_value, in_opt=dropdown1_value, out_opt=dropdown2_value, input_code=code, output_code=s)
+    response = openai.Completion.create(
+            engine="text-davinci-003",
+            prompt=s,
+            max_tokens=200,
+            n=1,
+    )
+    answer = response.choices[0].text.strip()
+    return render_template('result.html', bool=same_value, in_opt=dropdown1_value, out_opt=dropdown2_value, input_code=code, output_code=answer)
 
 
 if __name__ == '__main__':
